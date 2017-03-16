12cb130074c183c36b773375e6fa431f7131a2bb
jiajihe <jiajihe@thoughtworks.com>
2017-03-17 01:45:00 +0800

    add example

---
+++ Example/index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+import App from './App';
+
+const Example = () => {
+  return <App />;
+};
+
+AppRegistry.registerComponent('Example', () => Example);
